6975781e8420e5b7ebe8aeb743c95b0c687bc018
kaneki003 <sakshamchauhan707@gmail.com>
2024-05-15 01:44:21 +0530

    Corrected error in Onboarding

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -25,7 +25,7 @@ import { usePathname, useRouter } from "next/navigation";
 interface Props {
   user: {
     id: string;
-    objecttId: string;
+    objectId: string;
     username: string;
     name: string;
     bio: string;
